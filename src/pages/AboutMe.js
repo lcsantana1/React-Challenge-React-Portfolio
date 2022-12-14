376591bd63af0b2cca9ae00bc5929ec3dda4ad5b
lcsantana1 <lcsantana.pro@gmail.com>
2022-12-13 23:46:32 -0700

    added content to portfolio and about me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,18 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import portfolioImage from '../assets/self.png'
 
 export default function About() {
     return (
         <Container>
-            <h1>About ME</h1>
+            <h1>About</h1>
             <Row>
                 <Col>
-                    <img src="https://lcsantana1.github.io/first-challenge-hw/" alt='laura' width={75} />
+                    <img src={portfolioImage} alt='laura' width={300} />
                 </Col>
                 <Col>
-                    <p>I am a person who codes</p>
-                    <p>I am exhausted!</p>
+                    <p>Hello, welcome to my portfolio! My name is Laura Santana, I am currently enrolled in the U of U Full Stack Web Developer bootcamp. Currently we are learning the React technology, I am excited to continue learning and hoping to join a web developing team in the future!</p>
                 </Col>
             </Row>
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,19 +1,71 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import projectImage from '../assets/projectone.png'
+import projectTwoImage from '../assets/projecttwo.png'
+
 
 export default function Portfolio() {
     return (
         <Container>
-            <h1>my projects</h1>
+            <h1>My Projects</h1>
             <Row>
-                <Col>1 of 2</Col>
-                <Col>2 of 2</Col>
-            </Row>
-            <Row>
-                <Col>1 of 3</Col>
-                <Col>2 of 3</Col>
-                <Col>3 of 3</Col>
+                <Col>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={projectImage}/>
+                        <Card.Body>
+                            <Card.Title>Recipe Finder</Card.Title>
+                            <Card.Text>
+                            This website was a collaboration project to create a recipe website and SQL database that allows users to browse, post, and search for recipes create by a community of users.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                            <Button variant="primary">Go somewhere else</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">Go somewhere else</Button>
+                    </Card.Body>
+                </Card>
+                </Col>
+                <Col>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src= {projectTwoImage} />
+                    <Card.Body>
+                        <Card.Title>Weather Dashboard</Card.Title>
+                        <Card.Text>
+                        Welcome to the Weather Dashboard, in this application, the user will be able to look up different cities and see a five-day forecast that will include temperature, humidity, and the wind speed. Using the search bar, the user is also able to look up different cities and get updated five-day forecast for that city.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">Go somewhere else</Button>
+                    </Card.Body>
+                </Card>
+                </Col>
+                <Col>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">Go somewhere else</Button>
+                    </Card.Body>
+                </Card>
+                </Col>
             </Row>
         </Container>
     )
